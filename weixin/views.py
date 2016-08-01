de38b8655b53c85c4cf6c7243a99e45293fe6e29
Horton <wcbieyuan@gmail.com>
2016-08-01 18:54:24 +0800

    weixin 根据发送信息的类型进行回复

--- weixin/views.py
+++ weixin/views.py
@@ -8,7 +8,8 @@ from django.views.generic.base import View
 
 from wechat_sdk import WechatBasic, WechatConf
 from wechat_sdk.exceptions import ParseError
-from wechat_sdk.messages import TextMessage
+from wechat_sdk.messages import TextMessage, ImageMessage, VoiceMessage, VideoMessage, ShortVideoMessage, \
+    LocationMessage, LinkMessage, EventMessage
 
 
 import wx_config
@@ -44,17 +45,74 @@ class Weixin(View):
         except ParseError:
             return HttpResponse('Invalid Body Text')
 
-        id = wechat.message.id  # 对应于 XML 中的 MsgId
-        target = wechat.message.target  # 对应于 XML 中的 ToUserName
-        source = wechat.message.source  # 对应于 XML 中的 FromUserName
-        time = wechat.message.time  # 对应于 XML 中的 CreateTime
-        type = wechat.message.type  # 对应于 XML 中的 MsgType
-        raw = wechat.message.raw  # 原始 XML 文本，方便进行其他分析
+        # 公共信息
+        id = wechat.message.id              # 对应于 XML 中的 MsgId
+        target = wechat.message.target      # 对应于 XML 中的 ToUserName
+        source = wechat.message.source      # 对应于 XML 中的 FromUserName
+        time = wechat.message.time          # 对应于 XML 中的 CreateTime
+        type = wechat.message.type          # 对应于 XML 中的 MsgType
+        raw = wechat.message.raw            # 原始 XML 文本，方便进行其他分析
 
+        # 接收消息类型：
+        # 文字
         if isinstance(wechat.message, TextMessage):
-            content = wechat.message.content
+            content = wechat.message.content                # 对应于 XML 中的 Content
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        # 图片
+        elif isinstance(wechat.message, ImageMessage):
+            picurl = wechat.message.picurl                  # 对应于 XML 中的 PicUrl
+            media_id = wechat.message.media_id              # 对应于 XML 中的 MediaId
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
 
-        xml = wechat.response_text(content='Hello World!')
+        # 音频
+        elif isinstance(wechat.message, VoiceMessage):
+            media_id = wechat.message.media_id              # 对应于 XML 中的 MediaId
+            format = wechat.message.format                  # 对应于 XML 中的 Format
+            recognition = wechat.message.recognition        # 对应于 XML 中的 Recognition
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        # 视频
+        elif isinstance(wechat.message, (VideoMessage, ShortVideoMessage)):
+            media_id = wechat.message.media_id              # 对应于 XML 中的 MediaId
+            thumb_media_id = wechat.message.thumb_media_id  # 对应于 XML 中的 ThumbMediaId
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        # 地理位置
+        elif isinstance(wechat.message, LocationMessage):
+            location = wechat.message.location              # Tuple(X, Y)，对应于 XML 中的 (Location_X, Location_Y)
+            scale = wechat.message.scale                    # 对应于 XML 中的 Scale
+            label = wechat.message.label                    # 对应于 XML 中的 Label
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        # 链接
+        elif isinstance(wechat.message, LinkMessage):
+            title = wechat.message.title                    # 对应于 XML 中的 Title
+            description = wechat.message.description        # 对应于 XML 中的 Description
+            url = wechat.message.url                        # 对应于 XML 中的 Url
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        # 事件
+        elif isinstance(wechat.message, EventMessage):
+            if wechat.message.type == 'subscribe':          # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
+                key = wechat.message.key                    # 对应于 XML 中的 EventKey (普通关注事件时此值为 None)
+                ticket = wechat.message.ticket              # 对应于 XML 中的 Ticket (普通关注事件时此值为 None)
+            elif wechat.message.type == 'unsubscribe':      # 取消关注事件（无可用私有信息）
+                pass
+            elif wechat.message.type == 'scan':             # 用户已关注时的二维码扫描事件
+                key = wechat.message.key                    # 对应于 XML 中的 EventKey
+                ticket = wechat.message.ticket              # 对应于 XML 中的 Ticket
+            elif wechat.message.type == 'location':         # 上报地理位置事件
+                latitude = wechat.message.latitude          # 对应于 XML 中的 Latitude
+                longitude = wechat.message.longitude        # 对应于 XML 中的 Longitude
+                precision = wechat.message.precision        # 对应于 XML 中的 Precision
+            elif wechat.message.type == 'click':            # 自定义菜单点击事件
+                key = wechat.message.key                    # 对应于 XML 中的 EventKey
+            elif wechat.message.type == 'view':             # 自定义菜单跳转链接事件
+                key = wechat.message.key                    # 对应于 XML 中的 EventKey
+            elif wechat.message.type == 'templatesendjobfinish':  # 模板消息事件
+                status = wechat.message.status              # 对应于 XML 中的 Status
+            elif wechat.message.type in ['scancode_push', 'scancode_waitmsg', 'pic_sysphoto',
+                                         'pic_photo_or_album', 'pic_weixin', 'location_select']:  # 其他事件
+                key = wechat.message.key                    # 对应于 XML 中的 EventKey
+            xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+        else:
+            xml = wechat.response_text(content='你想要什么？')
 
         return HttpResponse(xml)
 
