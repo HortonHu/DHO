06c6c8231f446c1ff176aebc68e38e5d0cfb2101
Horton <wcbieyuan@gmail.com>
2016-08-03 11:14:37 +0800

    管理界面增加对对话、位置、功能

--- weixin/admin.py
+++ weixin/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 
-from .models import Fowler
+from .models import Fowler, Dialog, Location, Function
 
 
 class FowlerAdmin(admin.ModelAdmin):
-    list_display = ('OpenID', 'follow_time', 'location')
-    search_fields = ('OpenID', 'follow_time', 'location')
-    list_filter = ('follow_time', 'location')
+    list_display = ('OpenID', 'follow_time')
+    search_fields = ('OpenID', 'follow_time')
+    list_filter = ('follow_time',)
     date_hierarchy = 'follow_time'
 
     fields = ('location', )
 
 
 admin.site.register(Fowler, FowlerAdmin)
+admin.site.register(Dialog)
+admin.site.register(Location)
+admin.site.register(Function)
 
