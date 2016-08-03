b7c25760a19101ebb4ae5747c50a8c12ae91448c
Horton <wcbieyuan@gmail.com>
2016-08-03 11:12:37 +0800

    增加回复指定功能

--- weixin/models.py
+++ weixin/models.py
@@ -7,10 +7,35 @@ from django.db import models
 class Fowler(models.Model):
     OpenID = models.CharField(max_length=100, unique=True, verbose_name='用户ID')
     follow_time = models.DateTimeField(auto_now_add=True, verbose_name='关注时间')
-    location = models.CharField(max_length=100, default='None', verbose_name='地理位置')
 
     def __unicode__(self):
         return self.OpenID
 
 
+class Dialog(models.Model):
+    fowler = models.ForeignKey(Fowler)
+    message = models.CharField(max_length=200, verbose_name='用户发送信息')
+    reply = models.CharField(max_length=200, verbose_name='回复信息')
+    time = models.DateTimeField(auto_now=True, verbose_name='时间')
 
+    def __unicode__(self):
+        return self.message
+
+
+class Location(models.Model):
+    fowler = models.ForeignKey(Fowler)
+    x = models.FloatField(verbose_name='经度')
+    y = models.FloatField(verbose_name='纬度')
+    label = models.CharField(max_length=100, default='None', verbose_name='地理位置')
+    time = models.DateTimeField(auto_now=True, verbose_name='时间')
+
+    def __unicode__(self):
+        return self.label
+
+
+class Function(models.Model):
+    keyword = models.CharField(max_length=100, unique=True, verbose_name='功能关键字')
+    explain = models.CharField(max_length=200, verbose_name='功能解释')
+
+    def __unicode__(self):
+        return self.keyword
--- weixin/views.py
+++ weixin/views.py
@@ -6,9 +6,9 @@ from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 
-from models import Fowler
+from models import Fowler, Dialog, Location, Function
 
-from wechat_sdk import WechatBasic, WechatConf
+from wechat_sdk import WechatConf, WechatBasic
 from wechat_sdk.exceptions import ParseError
 from wechat_sdk.messages import TextMessage, LocationMessage, EventMessage
 
@@ -48,16 +48,30 @@ class Weixin(View):
         type = wechat.message.type          # MsgType
         raw = wechat.message.raw            # 原始 XML 文本
 
+        fowler = Fowler.objects.get_or_create(OpenID=source)
+
         if isinstance(wechat.message, TextMessage):
-            content = wechat.message.content.strip()                # 对应于 XML 中的 Content
-            response_xml = wechat.response_text(content='您发送的信息类型是{}'.format(type))
+            keywords = map(lambda x: x[0], Function.objects.all().values_list('keyword'))
+
+            content = wechat.message.content                # 对应于 XML 中的 Content
+            if content in keywords:
+                reply = Function.objects.get(keyword=content).explain
+            else:
+                reply = '本公众号支持的回复有： ' + ''.join(keywords)
+
+            dialog = Dialog(message=content, reply=reply, fowler=fowler)
+            dialog.save()
+            response_xml = wechat.response_text(content=reply)
+            return HttpResponse(response_xml)
+
         elif isinstance(wechat.message, LocationMessage):
             location = wechat.message.location              # Tuple(Location_X, Location_Y)
             scale = wechat.message.scale                    # 地图缩放大小
             label = wechat.message.label                    # 地理位置
-            user = Fowler.objects.get(OpenID=source)
-            user.location = label
-            user.save()
+
+            loc = Location(fowler=fowler, x=location[0], y=location[1], label=label)
+            loc.save()
+
         elif isinstance(wechat.message, EventMessage):
             if wechat.message.type == 'subscribe':          # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
                 new_fowler = Fowler(OpenID=source, follow_time=time)
@@ -66,15 +80,6 @@ class Weixin(View):
                 ticket = wechat.message.ticket              # 对应于 XML 中的 Ticket (普通关注事件时此值为 None)
                 response_xml = wechat.response_text(content='您发送的信息类型是{},已被添加到数据库'.format(type))
 
-            elif wechat.message.type == 'unsubscribe':      # 取消关注事件（无可用私有信息）
-                pass
-            elif wechat.message.type == 'scan':             # 用户已关注时的二维码扫描事件
-                key = wechat.message.key                    # 对应于 XML 中的 EventKey
-                ticket = wechat.message.ticket              # 对应于 XML 中的 Ticket
-            elif wechat.message.type == 'location':         # 上报地理位置事件
-                latitude = wechat.message.latitude          # 对应于 XML 中的 Latitude
-                longitude = wechat.message.longitude        # 对应于 XML 中的 Longitude
-                precision = wechat.message.precision        # 对应于 XML 中的 Precision
             elif wechat.message.type == 'click':            # 自定义菜单点击事件
                 key = wechat.message.key                    # 对应于 XML 中的 EventKey
             elif wechat.message.type == 'view':             # 自定义菜单跳转链接事件
@@ -84,10 +89,9 @@ class Weixin(View):
             elif wechat.message.type in ['scancode_push', 'scancode_waitmsg', 'pic_sysphoto',
                                          'pic_photo_or_album', 'pic_weixin', 'location_select']:  # 其他事件
                 key = wechat.message.key                    # 对应于 XML 中的 EventKey
-
         else:
             response_xml = wechat.response_text(content="回复'功能'了解本公众号提供的查询功能")
-        return HttpResponse(response_xml)
+            return HttpResponse(response_xml)
 
 
 
